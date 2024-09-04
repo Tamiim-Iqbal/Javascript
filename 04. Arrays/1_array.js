f3fb41b454e47c2ee34e00d652c7a60db216de48
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2024-09-04 21:45:16 +0600

    Arrays

---
+++ 04. Arrays/1_array.js
@@ -0,0 +1,28 @@
+// Arrays : Collection of items
+
+// let heroes = ["Spiderman", "Batman", "Superman", "Ironman"];
+// console.log(heroes);
+// // console.log(typeof heroes);
+// // console.log(marks.length);
+
+// Indices
+// console.log(heroes[0]);                         //output: Spiderman
+// console.log(heroes[1]);                         //output: Batman
+// console.log(heroes[3]);                         //output: Ironman
+// console.log(heroes[10]);                        //output: undefined
+
+
+let marks = [80, 87, 98, 90, 68, 42];
+// console.log(marks);
+
+// for loop
+// for (let i = 0; i < marks.length; i++)
+// {
+//     console.log(marks[i]);
+// }
+
+// for of loop
+for (let mark of marks)
+{
+    console.log(mark);
+}
