5714c795a63630200a720169007e674d03fee592
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2024-09-06 17:10:04 +0600

    Function & Methods

---
+++ 05. Function & Methods/1_function.js
@@ -0,0 +1,30 @@
+// // Function : Block of code that performs a specific task, can be invoked whenever needed.
+
+// function myFun()
+// {
+//     console.log("We are learning JS");
+// }
+// myFun();
+
+// // Function with Parameter(input)
+// function myMsg(msg, msg2)
+// {
+//     console.log(msg, msg2);
+// }
+// myMsg("Hello", "JS");     //Argument
+
+// // Return Keyword
+// function sum (x, y)
+// {
+//     s = x + y;
+//     return s;
+// }
+// let ans = sum(4, 7);
+// console.log(ans);
+
+// Arrow Function : Compact way of writing a function
+const arrowFun = (a, b) =>
+{
+    console.log(a * b);
+}
+arrowFun(2,9);
---
+++ 05. Function & Methods/2_for_each_loop.js
@@ -0,0 +1,8 @@
+// forEach(callBackFunction)
+// a callback function is a function passed as an argument to another function 
+
+let array = [1, 2, 3, 4, 5];
+
+array.forEach((val) => {
+    console.log(val);
+})
